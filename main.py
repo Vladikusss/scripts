f51071b01e7a342a61e72755f298994e7a926910
Vladdy <182237916+Vladikusss@users.noreply.github.com>
2025-02-28 13:04:01 +0000

    Abandoned project, used the wrong approach, lost interest

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ e.g. morning, day, dinner.
 ---> Output if meals searched have any allergens, and if so which ones.
 ---> The user can then choose the meal from the list searched, and the program will output the information about the meal.
 
- *** Own Reference: Fix the loop in the class, add _str_ to the Meal class, fix any other issues
  */
 """
 
@@ -28,25 +27,52 @@ class Meal:
         self.time = best_served
         self.allergens = allergens
 
+    def __str__(self):
+        """Function to specify how to output Meal objects."""
+        return (f"Name: {self.name} | "
+                f"Ingredients: {self.ingredients} | "
+                f"Time to serve: {self.time} | "
+                f"Allergens: {self.allergens}")
+
     def PrintMealDetails(self):
+        """Function to print out the details about the meal."""
         print(f"Recipe: {self.name}")
         print(f"Ingredients:", ", ".join(self.ingredients))
         print(f"Time to serve: {self.time}")
         print(f"Allergens:", ", ".join(self.allergens))
 
 class RecipeManager:
+    """A class to manage all meals."""
     def __init__(self):
         self.meals = list()
 
-    def AddMeal(self, meal):
+    def AddMeal(self, meal: object):
+        """Function to add a meal."""
         self.meals.append(meal)
 
-    def SearchByTimeOfDay(self, time_of_day):
+    def SearchByTimeOfDay(self, time_of_day: str):
+        """Function to search and filter out meals by the time of the day."""
+        found = False
         for meal in self.meals:
-            if time_of_day == meal.time:
-                print("It works :)))")
+            if time_of_day.lower() == meal.time.lower():
+                print(f"- {meal}")
+                found = True
+        if not found:
+            print("We don't have any meals for the given time.")
+
+    def SearchByIngredients(self, ingredients: list[str]):
+        found = False
+        for meal in self.meals:
+            if all(ingrs in ingredients for ingrs in meal.ingredients):
+                print(f"- {meal.name} | Contains: {meal.allergens}")
+                found = True
+        if not found:
+            print("We don't have any meals with these ingredients")
+
+
 
     def PrintMeals(self):
+        """Function to print all available meals."""
         if not self.meals:
             print("No meals available.")
         else:
@@ -63,19 +89,20 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
     # For testing:
-    Meal1 = Meal("example", ["list", "of", "ingredients"], "day", ["prawn", "crabs"])
+    Meal1 = Meal("pasta", ["milk", "bread", "water"], "day", ["prawn", "crabs"])
     Meal1.PrintMealDetails()
     print(Meal1.allergens)
 
     recipeManager = RecipeManager()
     # Add a meal
     test = recipeManager.AddMeal(Meal1)
-    print(test)
     # Search for meals by time of day
     test = recipeManager.SearchByTimeOfDay("day")
     print(test)
     # Print all stored meals
     test = recipeManager.PrintMeals()
+    print(test)
+    test = recipeManager.SearchByIngredients(["milk", "bread", "water"])
     print(test)
 
 
